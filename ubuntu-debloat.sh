5d0ee0571ffd5e871e84369069486b8934dbbfdf
ATik HaSan <atikhasan2700@gmail.com>
2026-02-11 02:46:42 +0600

    Remove Firefox from Ubuntu debloat script
    Remove Firefox along with other applications during debloating.

--- ubuntu-debloat.sh
+++ ubuntu-debloat.sh
@@ -1,1 +1,2 @@
 sudo apt remove -y gnome-clocks gnome-calculator yelp baobab gnome-logs evince gnome-software language-selector-gnome gnome-characters && sudo apt autoremove -y && sudo apt autoclean
+sudo apt remove -y firefox && sudo snap remove firefox 2>/dev/null || true
