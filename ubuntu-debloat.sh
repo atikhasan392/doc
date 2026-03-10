c16a88b0e7e638fa112d1894c3630c4942510d3d
ATik HaSan <atikhasan2700@gmail.com>
2026-03-10 22:26:44 +0600

    Delete ubuntu-debloat.sh

--- ubuntu-debloat.sh
+++
@@ -1,3 +0,0 @@
-sudo apt purge -y gnome-clocks gnome-calculator baobab gnome-logs gnome-characters evince yelp && sudo snap remove firefox && sudo apt purge -y firefox && sudo apt autoremove --purge -y && sudo apt autoclean
-
-sudo apt install diodon
